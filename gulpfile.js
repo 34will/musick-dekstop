2ff732e1f0c1c0e375a47eaede8bb1c94d6ba526
William Gilmour <wgilmour@hotmail.co.uk>
2016-12-23 18:49:33 +0000

    Added Ahead-of-Time compilation.

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,9 @@
 var embedTemplates = require("gulp-angular-embed-templates");
+var exec = require("child_process").exec;
 var gulp = require("gulp");
 var plumber = require("gulp-plumber");
 var sass = require("gulp-sass");
+var sequence = require('run-sequence');
 var tsc = require("gulp-typescript");
 var watch = require("gulp-watch");
 
@@ -16,7 +18,7 @@ function handleError(err) {
 	this.emit('end');
 }
 
-function compileAngular() {
+gulp.task("angular.compile", function () {
 	var options = {
 		basePath: "./",
 		minimize: { quotes: true },
@@ -28,22 +30,38 @@ function compileAngular() {
 		.pipe(embedTemplates(options))
 		.pipe(tsProject())
 		.pipe(gulp.dest("./" + pathBuild + "/"));
-}
-
-gulp.task("angular.compile", compileAngular);
+});
 
 gulp.task("angular.watch", function () {
 	return watch(["./" + pathSource + "/**/*.html", "./" + pathSource + "/**/*.ts"], { read: false }, function () { gulp.start("angular.compile"); });
 });
 
-function compileSCSS() {
+gulp.task("angular.ngc", function (cb) {
+	return exec("node_modules/.bin/ngc -p tsconfig-aot.json", function (err, stdout, stderr) {
+		console.log(stdout);
+		console.log(stderr);
+		cb(err);
+	});
+});
+
+gulp.task("angular.rollup", function (cb) {
+	return exec("node_modules/.bin/rollup -c rollup-config.js", function (err, stdout, stderr) {
+		console.log(stdout);
+		console.log(stderr);
+		cb(err);
+	});
+});
+
+gulp.task("angular.aot-compile", function () {
+	return sequence("angular.ngc", "angular.rollup");
+});
+
+gulp.task("scss.compile", function () {
 	return gulp.src("./" + pathStyles + "/main.scss")
 		.pipe(plumber({ errorHandler: handleError }))
 		.pipe(sass().on("error", sass.logError))
 		.pipe(gulp.dest("./" + pathStyles + "/"));
-}
-
-gulp.task("scss.compile", compileSCSS);
+});
 
 gulp.task("scss.watch", function () {
 	return watch("./" + pathStyles + "/**/*.scss", { read: false }, function () { gulp.start("scss.compile"); });
